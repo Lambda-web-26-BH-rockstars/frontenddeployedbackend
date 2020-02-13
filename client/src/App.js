9bb5f0c5419931a133a78931f879a5997f0d2856
Brian Hague <alphaseinor@gmail.com>
2020-02-13 17:22:21 -0600

    add, and update works

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,14 @@
 import React, {useEffect, useState} from 'react';
 import axios from 'axios'
 import User from './User';
+import AddName from './AddName';
+
+import {APIContext} from './contexts/APIContext'
 
 function App() {
   const [api] = useState('https://zakmayfield-api3.herokuapp.com/api/users')
   const [getDb, setGetDb] = useState([])
+  const [reset, setReset] = useState(true)
 
   useEffect(()=>{
     axios.get(api)
@@ -13,15 +17,26 @@ function App() {
         setGetDb(res.data)
       })
       .catch(err => console.log(err))
-  },[])
+  },[reset])
+
+  const resetHandler = () => {
+    setReset(!reset)
+  }
 
   return (
+    <APIContext.Provider value={{api:api, resetHandler:resetHandler}}>
     <>
       <header></header>
       <main>
-        {getDb.map((user, i)=> <User key={i} user={user} /> )}
+        <section>
+          <AddName />
+        </section>
+        <section>
+          {getDb.map((user, i)=> <User key={i} user={user} /> )}
+        </section>
       </main> 
     </>
+    </APIContext.Provider>
   );
 }
 
--- client/src/User.js
+++ client/src/User.js
@@ -1,11 +1,76 @@
-import React from 'react'
+import React, {useState, useContext} from 'react'
+import {APIContext} from './contexts/APIContext'
+import axios from 'axios'
+
 
 const User = ({user}) => {
+  const [name, setName] = useState({name: user.name})
+  const [editing, setEditing] = useState(false)
+
+  const {
+    api,
+    resetHandler
+  } = useContext(APIContext)
+
+
+  const changeHandler = (e) => {
+    setName({...name, [e.target.name] : e.target.value})
+  }
+
+  const editHandler = (e) => {
+    e.preventDefault()
+    console.log(`${api}/${user.id}`, name)
+    axios.put(`${api}/${user.id}`, name)
+      .then(res => console.log(res))
+      .catch(err => console.log(err))
+      .finally(() => {
+        editToggle()
+        resetHandler()
+      })
+  }
+
+  const editToggle = ( ) => {
+    setEditing(!editing)
+  }
+
   return (
     <>
-      <div>
-        <p>{user.name}</p>
-      </div>
+      <article>
+        {editing?(
+          <form
+            onSubmit={(e)=> editHandler(e)}
+          >
+            <label>Edit Name</label>
+            <input 
+              name="name"
+              value={name.name}
+              onChange={(e)=>changeHandler(e)}
+            />
+            <button type="submit">Edit Name</button>
+        </form>
+        ):(
+        <p>{user.name}
+
+        </p>)}
+        <button
+            onClick={
+              ()=> {
+                editToggle()
+              }
+            }
+          >
+            Edit Name
+          </button>
+          <button
+            onClick={
+              ()=> {
+                console.log("delete", user.name, user.id)
+              }
+            }
+          >
+            Delete User
+          </button>
+      </article>
     </>
   )
 }
